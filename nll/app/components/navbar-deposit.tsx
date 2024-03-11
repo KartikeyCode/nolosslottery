14dee0f1aaacdcf1250293af9aa77285049fe5ff
KartikeyCode <codekartikey@gmail.com>
2024-03-11 18:47:03 +0530

    add AptosWalletConnector

--- nll/app/components/navbar-deposit.tsx
+++ nll/app/components/navbar-deposit.tsx
@@ -1,17 +1,23 @@
+import "./wallet.css"
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 import Image from "next/image";
 import logo from "@/public/logo.png"
-export default function NavbarDeposit() {
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
+export default function Navbar() {
   return (
-   <div className="h-14 ml-14 pt-9  flex  items-center bg-[#F4F4FC]">
-     <Image className="self-center" src={logo} alt="logo"/>
-     <div className="flex gap-8 self-center md:ml-[9rem]  xl:ml-[19rem] 2xl:ml-[38rem] ">
+   <div className="h-14 ml-14 pt-9  flex  items-center justify-between bg-[#F4F4FC] ">
+     <Image className="" src={logo} alt="logo"/>
+     <div className="flex gap-8 self-center ">
         <a className="text-[#979DAA] text-lg font-semibold font-Urbanist" href="/"> Home </a>
-        <a className="text-black text-lg font-semibold font-Urbanist" href="/deposit"> Deposit </a>
+        <a className=" text-black text-lg font-semibold font-Urbanist" href="/deposit"> Deposit </a>
         <a className="text-[#979DAA] text-lg font-semibold font-Urbanist" href="/leaderboard"> Leaderboard </a>
         <a className="text-[#979DAA] text-lg font-semibold font-Urbanist" href="/faq"> FAQ </a>
         <a className="text-[#979DAA] text-lg font-semibold font-Urbanist" href="/refer"> Refer </a>
         <a className="text-[#979DAA] text-lg font-semibold font-Urbanist" href="/docs"> Docs </a>
      </div>
+     
+     <WalletSelector/>
+   
    </div>
   );
 }
--- nll/app/components/navbar-home.tsx
+++ nll/app/components/navbar-home.tsx
@@ -1,10 +1,13 @@
+import "./wallet.css"
+import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 import Image from "next/image";
 import logo from "@/public/logo.png"
+import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 export default function Navbar() {
   return (
-   <div className="h-14  ml-14 pt-9 flex  items-center bg-white ">
+   <div className="h-14 ml-14 pt-9  flex  items-center justify-between bg-white" >
      <Image className="" src={logo} alt="logo"/>
-     <div className="flex gap-8 self-center md:ml-[9rem]  xl:ml-[19rem] 2xl:ml-[38rem] ">
+     <div style={{zIndex:1}} className="flex gap-8 self-center ">
         <a className="text-black text-lg font-semibold font-Urbanist" href="/"> Home </a>
         <a className="text-[#979DAA] text-lg font-semibold font-Urbanist" href="/deposit"> Deposit </a>
         <a className="text-[#979DAA] text-lg font-semibold font-Urbanist" href="/leaderboard"> Leaderboard </a>
@@ -12,6 +15,9 @@ export default function Navbar() {
         <a className="text-[#979DAA] text-lg font-semibold font-Urbanist" href="/refer"> Refer </a>
         <a className="text-[#979DAA] text-lg font-semibold font-Urbanist" href="/docs"> Docs </a>
      </div>
+     
+     <WalletSelector/>
+   
    </div>
   );
 }
